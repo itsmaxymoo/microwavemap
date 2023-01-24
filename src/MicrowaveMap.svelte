99957ad5f21bdd5ccba8df868cee35b5e28fe35d
itsmaxymoo <max.loiacono@protonmail.com>
2023-01-23 22:45:54 -0500

    microwave location popup

--- src/MicrowaveMap.svelte
+++ src/MicrowaveMap.svelte
@@ -4,8 +4,22 @@
     import MapMarker from "./lib/leaflet/MapMarker.svelte";
 </script>
 
-<Map initialLatLng={[40.800901, -77.86094]} initialZoomLevel={17} height=844px width=100%>
-    {#each locations as loc }
-        <MapMarker lat={loc.lat} lng={loc.lng} />
+<Map
+    initialLatLng={[40.800901, -77.86094]}
+    initialZoomLevel={17}
+    height="844px"
+    width="100%"
+>
+    {#each locations as loc}
+        <MapMarker lat={loc.lat} lng={loc.lng}>
+            <p class="has-text-weight-semibold is-size-6">{loc.building}</p>
+            <p>
+                {#if loc.room_exact}
+                    Room: {loc.room_exact}
+                {:else}
+                    {loc.room_approx}
+                {/if}
+            </p>
+        </MapMarker>
     {/each}
 </Map>
--- src/lib/leaflet/MapMarker.svelte
+++ src/lib/leaflet/MapMarker.svelte
@@ -3,12 +3,22 @@
 	import L from "leaflet";
 	import { getContext, onMount } from "svelte";
 
+	let popupBodyContent: HTMLElement;
+
 	export let lat: number = 0;
 	export let lng: number = 0;
 
 	const map: L.Map = getContext("leaflet-map");
 
 	onMount(() => {
-		let marker: L.Marker = L.marker(new L.LatLng(lat, lng)).addTo(map);
+		let marker: L.Marker = L.marker(new L.LatLng(lat, lng));
+		if (popupBodyContent.innerHTML) {
+			marker.bindPopup(popupBodyContent.innerHTML);
+		}
+		marker.addTo(map);
 	});
 </script>
+
+<div bind:this={popupBodyContent} hidden>
+	<slot />
+</div>
