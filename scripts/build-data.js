f820eb29c07327eda3cdfd3ff704370524cab7ca
itsmaxymoo <max.loiacono@protonmail.com>
2023-01-23 21:31:32 -0500

    format build data

--- scripts/build-data.js
+++ scripts/build-data.js
@@ -8,11 +8,11 @@ let input_files = fs.readdirSync(input_path).map(inf => input_path + "/" + inf);
 input_files = input_files.map(file => fs.readFileSync(file));
 
 class _Location {
-	constructor(building, room_exact, room_approx, coords){
+	constructor(building, room_exact, room_approx, coords) {
 		this.building = building;
 		this.room_exact = room_exact;
 		this.room_approx = room_approx;
-		
+
 		let coordArray = coords.split(/\s*,\s*/);
 
 		this.lat = parseFloat(coordArray[0]);
